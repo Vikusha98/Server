8c56bb7ac1c098f17889231a380bfbc5008505bf
Vikusha <victoriaflint01@gmail.com>
2024-06-06 22:50:50 +0300

    Parameters and name added

--- src/main/java/ru/netology/Server.java
+++ src/main/java/ru/netology/Server.java
@@ -1,14 +1,20 @@
 package ru.netology;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
 public class Server {
     private final List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png",
@@ -43,7 +49,16 @@ public class Server {
                 return;
             }
 
-            final var path = parts[1];
+            final var url = parts[1];
+            final var path = url.split("\\?")[0]; // Получаем путь, отделяя параметры запроса
+            final var params = URLEncodedUtils.parse(url, Charset.forName("UTF-8")); // Парсим строку запроса на параметры
+
+            final var nameParam = getQueryParam(params, "name");
+            System.out.println("Value of 'name' parameter: " + nameParam);
+
+            final var allParams = getQueryParams(params);
+            System.out.println("All query parameters: " + allParams);
+
             if (!validPaths.contains(path)) {
                 out.write((
                         "HTTP/1.1 404 Not Found\r\n" +
@@ -90,5 +105,19 @@ public class Server {
             e.printStackTrace();
         }
     }
-}
 
+    // name
+    public String getQueryParam(List<NameValuePair> params, String name) {
+        return params.stream()
+                .filter(param -> param.getName().equals(name))
+                .findFirst()
+                .map(NameValuePair::getValue)
+                .orElse(null);
+    }
+
+    // params
+    public Map<String, String> getQueryParams(List<NameValuePair> params) {
+        return params.stream()
+                .collect(Collectors.toMap(NameValuePair::getName, NameValuePair::getValue));
+    }
+}
